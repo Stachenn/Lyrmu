c71f6ec043d27ea7ec17690f44b4bec748b44aff
Stanislaw <stanislawjano23@gmail.com>
2024-08-21 00:44:24 +0200

    refactory: change from cli curl to libcurl

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,68 @@
 #include "../include/jsonParser/dynamicArray.cpp"
 #include "../include/jsonParser/parser.cpp"
-
+#include <curl/curl.h>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
 
+size_t reciveData(char* recivedData, size_t size, size_t nmemb, std::string varStoragedData){
+    varStoragedData += recivedData;
+    return 0;
+}
+
 int main(int argc, char* argv[]){
-    system("chcp 65001");
+    #ifdef _WIN32 || _WIN64
+        system("chcp 65001");
+    #endif
+
     std::string songName = "";
-    std::string jsonFromSite = "";
-	
+    std::string jsonFromSite;
+
     std::vector <std::string> values;
     std::vector <std::string> names;
-	std::ifstream site;
+
+    CURL *curl = curl_easy_init();
+    CURLINFO info;
+    CURLcode res;
+
     jsonParser test("");
+
     std::cout << "type name of song";
     std::cin >> songName;
-    system("del cache.txt");
-    system(std::string("curl https://lrclib.net/api/search?q=" + songName + " >> cache.txt").c_str());
-    
-    site.open("cache.txt", std::ios::ate);
-    
-    int siteSize = site.tellg();
+    if (curl){
 
-    site.seekg(0, std::ios::beg);
-    
-    for (int i = 0; i < siteSize; i++){
-        jsonFromSite += site.get();
+        curl_easy_setopt(curl, CURLOPT_URL, std::string("https://lrclib.net/api/search?q=" + songName).c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, reciveData);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &jsonFromSite);
+        curl_easy_setopt(curl, CURLOPT_USERAGENT, "curl/1.0");
+
+        curl_easy_perform(curl);
+
+        test.content = jsonFromSite;
+        curl_easy_cleanup(curl);
     }
     
-    test.content = jsonFromSite;
+    else{
+        res = curl_easy_getinfo(curl, info, CURLINFO_HTTP_CONNECTCODE);
+        std::cout << res;
+        std::cout << "Error: connection with api failed (maybe)";
+    }
+    
     test.parse();
     values = test.getValues();
     names = test.getNames();
 
-    int a = test.find("\"plainLyrics\"");
-
+    int a = test.find("plainLyrics");
+    std::cout << a;
 	std::cout << values[8];
-    if (a == -1){
-        //std::cout << 'a';
-		site.close();
-        return 0;
-    }
-    
 
-    site.close();
+    if (a == -1){
+        std::cout << "Error: can not find song / api changed";
+        return -1;
+    }
+
+
+    //site.close();
     return 0;
 }
