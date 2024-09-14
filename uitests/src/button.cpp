f532fb0517a8093fc1c86dafd4af66bec37e8118
Stanislaw <stanislawjano23@gmail.com>
2024-09-14 23:49:37 +0200

    fix:SDL_Event event is now given to isClicked arg What fix segmentation fault(core dumped) and detecting events

--- uitests/src/button.cpp
+++ uitests/src/button.cpp
@@ -4,11 +4,8 @@
 #include <SDL2/SDL_image.h>
 #include "button.hpp"
 
-button::button(int posX, int posY, int sizeX, int sizeY, SDL_Window* windo){
-    shape.x = posX;
-    shape.y = posY;
-    shape.w = sizeX;
-    shape.h = sizeY;
+button::button(SDL_Rect shap, SDL_Window* windo, const char* pngTexturePath){
+    shape = shap;
     window = windo;
 }
 
@@ -16,23 +13,16 @@ button::~button(){
     SDL_DestroyTexture(texture);
 }
 
-int button::isClicked(){
-    //SDL_PollEvent(&event);
-    //std::cout << event.type;
-    /*if (event.type == SDL_MOUSEBUTTONEVENT){
-        if (SDL_GetWindowID(window) == event.button.windowID){
-            if (event.button.type == SDL_MOUSEBUTTONDOWN){
-                
-                if (event.button.x < (shape.x + shape.w) && event.button.x > shape.x){
-                    if (event.button.y < (shape.y + shape.h) && event.button.y > shape.y){
-                        return 1;
-                    }
+int button::isClicked(SDL_Event event){ 
+    if (event.type == SDL_MOUSEBUTTONDOWN){
+        if (SDL_GetWindowID(window) == event.button.windowID){         
+            if (event.button.x < (shape.x + shape.w) && event.button.x > shape.x){
+                if (event.button.y < (shape.y + shape.h) && event.button.y > shape.y){
+                    return 1;
                 }
-                
             }
         }
     }
-    */
     return 0;
 }
 
--- uitests/src/button.hpp
+++ uitests/src/button.hpp
@@ -7,7 +7,6 @@
 
 class button{
     private:
-        SDL_Event event;
         SDL_Texture* texture = nullptr;
         SDL_Window* window = nullptr;
         std::string filePath = "";
@@ -15,11 +14,11 @@ class button{
     public:
         SDL_Rect shape = {0, 0, 0, 0};
 
-        button(int posX, int posY, int sizeX, int sizeY, SDL_Window* windo);
+        button(SDL_Rect shap, SDL_Window* windo, const char* pngTexturePath);
         ~button();
 
-        int isClicked();
-        int changeTexuture(std::string newPath);
+        int isClicked(SDL_Event event);
+        int changeTexuture(const char* newPath);
         
         void fadeOut(int speed); //SDL_SetTextureAlphaMod(texture, 0);
         void fadeIn(int speed);
