de7bffe507437d463687e6b7baa5fa6d9e5a2b47
Stanislaw <stanislawjano23@gmail.com>
2024-10-28 16:05:34 +0100

    feat: Prepared files that will be render scenes

---
+++ uitests/src/scenes.cpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include "scenes.hpp"
+
+void bar(SDL_Window* window, SDL_Renderer* renderer, SDL_Color backgroundColor){
+
+}
+
+void home(SDL_Window* window, SDL_Renderer* renderer, SDL_Color backgroundColor){
+
+}
+
+void library(SDL_Window* window, SDL_Renderer* renderer, SDL_Color backgroundColor){
+
+}
+
+void settings(SDL_Window* window, SDL_Renderer* renderer, SDL_Color backgroundColor){
+
+}
+
+void random(SDL_Window* window, SDL_Renderer* renderer, SDL_Color backgroundColor){
+
+}
+
+void search(SDL_Window* window, SDL_Renderer* renderer, SDL_Color backgroundColor){
+
+}
+
---
+++ uitests/src/scenes.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+
+void bar(SDL_Window* window, SDL_Renderer* renderer, SDL_Color backgroundColor);
+void search(SDL_Window* window, SDL_Renderer* renderer, SDL_Color backgroundColor);
+void settings(SDL_Window* window, SDL_Renderer* renderer, SDL_Color backgroundColor);
+void library(SDL_Window* window, SDL_Renderer* renderer, SDL_Color backgroundColor);
+void home(SDL_Window* window, SDL_Renderer* renderer, SDL_Color backgroundColor);
+void random(SDL_Window* window, SDL_Renderer* renderer, SDL_Color backgroundColor);
+
