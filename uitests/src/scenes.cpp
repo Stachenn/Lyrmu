ed582ec91ba171d1d0ae5e4e0dba62125eb46fa1
Stanislaw <stanislawjano23@gmail.com>
2024-10-29 19:59:14 +0100

    feat: Changed from function to class create render func for 'bar', etc.

--- uitests/src/scenes.cpp
+++ uitests/src/scenes.cpp
@@ -5,27 +5,99 @@
 #include <SDL2/SDL_ttf.h>
 #include "scenes.hpp"
 
-void bar(SDL_Window* window, SDL_Renderer* renderer, SDL_Color backgroundColor){
+scenes::scenes(SDL_Window* windo, SDL_Renderer* rendere, SDL_Color backgroundColo, bool debugMod, int windowSizY, int windowSizX){
+    this->window = windo;
+    this->renderer = rendere;
+    this->backgroundColor = backgroundColo;
+        
+    this->debugMode = debugMod;
+    this->windowSizeY = windowSizY; 
+    this->windowSizeX = windowSizX;
+
+    homet = IMG_LoadTexture(renderer, "res/icons/home.png");
+    searcht = IMG_LoadTexture(renderer, "res/icons/search.png");
+    randomt = IMG_LoadTexture(renderer, "res/icons/random.png");
+    libraryt = IMG_LoadTexture(renderer, "res/icons/library.png");
+    settingst = IMG_LoadTexture(renderer, "res/icons/settings.png");
+    bart = IMG_LoadTexture(renderer, "res/interface/bar.png");
+}
+
+scenes::~scenes(){
+    SDL_DestroyTexture(homet);
+    SDL_DestroyTexture(randomt);
+    SDL_DestroyTexture(searcht);
+    SDL_DestroyTexture(libraryt);
+    SDL_DestroyTexture(settingst);
+    SDL_DestroyTexture(bart);
+}
+void scenes::baar(){
+    SDL_Rect iconssize = {0, 0, 0, 0};
+    SDL_Rect barsize = {0, 0, 0, 0};
+
+    barsize.w = 1400;
+    barsize.h = 170;
+    barsize.x = (windowSizeX - barsize.w) / 2;
+    barsize.y = windowSizeY - 190;
+
+    iconssize.w = 252;
+    iconssize.h = 162;
+    iconssize.x = 110;
+    iconssize.y = barsize.y;
+
+    SDL_SetRenderDrawColor(renderer, backgroundColor.r, backgroundColor.g, backgroundColor.b, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_RenderCopy(renderer, bart, NULL, &barsize);
+            
+    SDL_RenderCopy(renderer, randomt, NULL, &iconssize);
+    iconssize.x += iconssize.w + 25;
+
+    SDL_RenderCopy(renderer, searcht, NULL, &iconssize);
+    iconssize.x += iconssize.w + 25;
+
+    SDL_RenderCopy(renderer, homet, NULL, &iconssize);
+    iconssize.x += iconssize.w + 25;
+
+    SDL_RenderCopy(renderer, libraryt, NULL, &iconssize);
+    iconssize.x += iconssize.w + 25;
+            
+    SDL_RenderCopy(renderer, settingst, NULL, &iconssize);
+            
+    if (debugMode){
+        iconssize.x = 110;
+
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        for (int i = 0; i < 5; i++){
+            SDL_RenderDrawRect(renderer, &iconssize);
+                    
+            iconssize.x += iconssize.w + 25;
+        }
+        SDL_SetRenderDrawColor(renderer, backgroundColor.r, backgroundColor.g, backgroundColor.b, 255);
+    }
+    iconssize.x = 110;
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+    //SDL_RenderDrawRect(renderer, &background);
+    SDL_RenderPresent(renderer);
+}
+
+void scenes::home(){
 
 }
 
-void home(SDL_Window* window, SDL_Renderer* renderer, SDL_Color backgroundColor){
+void scenes::library(){
 
 }
 
-void library(SDL_Window* window, SDL_Renderer* renderer, SDL_Color backgroundColor){
+void scenes::settings(){
 
 }
 
-void settings(SDL_Window* window, SDL_Renderer* renderer, SDL_Color backgroundColor){
+void scenes::random(){
 
 }
 
-void random(SDL_Window* window, SDL_Renderer* renderer, SDL_Color backgroundColor){
-
-}
-
-void search(SDL_Window* window, SDL_Renderer* renderer, SDL_Color backgroundColor){
+void scenes::search(){
 
 }
 
--- uitests/src/scenes.hpp
+++ uitests/src/scenes.hpp
@@ -4,10 +4,30 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
 
-void bar(SDL_Window* window, SDL_Renderer* renderer, SDL_Color backgroundColor);
-void search(SDL_Window* window, SDL_Renderer* renderer, SDL_Color backgroundColor);
-void settings(SDL_Window* window, SDL_Renderer* renderer, SDL_Color backgroundColor);
-void library(SDL_Window* window, SDL_Renderer* renderer, SDL_Color backgroundColor);
-void home(SDL_Window* window, SDL_Renderer* renderer, SDL_Color backgroundColor);
-void random(SDL_Window* window, SDL_Renderer* renderer, SDL_Color backgroundColor);
+class scenes{
+    private:
+        SDL_Texture *homet = NULL;
+        SDL_Texture *searcht = NULL;
+        SDL_Texture *randomt = NULL;
+        SDL_Texture *libraryt = NULL;
+        SDL_Texture *settingst = NULL;
+        SDL_Texture *bart = NULL;
 
+        SDL_Window* window = NULL;
+        SDL_Renderer* renderer = NULL;
+        SDL_Color backgroundColor;
+        
+        bool debugMode = false;
+        int windowSizeY = 0; 
+        int windowSizeX = 0;
+    public:
+        scenes(SDL_Window* windo, SDL_Renderer* rendere, SDL_Color backgroundColo, bool debugMod, int windowSizY, int windowSizX);
+        ~scenes();
+
+        void baar();
+        void search();
+        void settings();
+        void library();
+        void home();
+        void random();
+};
