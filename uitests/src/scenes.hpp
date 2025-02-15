4df8d275f32783815cf0b2cddb1727eb3149128d
Stachenn <stanislawjano23@gmail.com>
2025-02-15 17:40:04 +0100

    Fix+Feat(): Setting NULL to ptr changed to nullptr, Added renderScene func

--- uitests/src/scenes.hpp
+++ uitests/src/scenes.hpp
@@ -3,33 +3,54 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
+#include "../include/sdlfont/font.cpp"
+#include <vector>
 
 class scenes{
     private:
-        SDL_Texture *homet = NULL;
-        SDL_Texture *searcht = NULL;
-        SDL_Texture *randomt = NULL;
-        SDL_Texture *libraryt = NULL;
-        SDL_Texture *settingst = NULL;
-        SDL_Texture *bart = NULL;
+        std::vector<SDL_Surface*> surfaces = {};
+        std::vector<SDL_Texture*> textures = {};
+        std::vector<int> lastSceneIndexes = {};
 
-        SDL_Window* window = NULL;
-        SDL_Renderer* renderer = NULL;
-        SDL_Color backgroundColor;
-        
+        SDL_Color red = {255, 0, 0};
+
+        SDL_Texture *homet = nullptr;
+        SDL_Texture *searcht = nullptr;
+        SDL_Texture *randomt = nullptr;
+        SDL_Texture *libraryt = nullptr;
+        SDL_Texture *settingst = nullptr;
+        SDL_Texture *bart = nullptr;
+
+        SDL_Window* window = nullptr;
+        SDL_Renderer* renderer = nullptr;
+
+        SDL_Texture* cacheTxt = nullptr;
+        SDL_Surface* cacheSurf = nullptr;
+        TTF_Font* testFont = nullptr;
+
+        int lastSceneIndex = 255;
+        //textDisplay* sahabek = nullptr;
+
+    public:
+        SDL_Color backgroundColor = {0,0,0,0};
+        SDL_Rect testRect = {500, 500, 100, 100};
         bool debugMode = false;
-        int windowSizeY = 0; 
+        bool objectDestroyed = false;
+
+        int windowSizeY = 0;
         int windowSizeX = 0;
         int iconScale = 1;
 
-    public:
         scenes(SDL_Window* windo, SDL_Renderer* rendere, SDL_Color backgroundColo, bool debugMod, int windowSizY, int windowSizX, int iconScal);
         ~scenes();
 
-        void baar();
+        void bar();
         void search();
         void settings();
         void library();
         void home();
-        void random();
+        void random(); // 5 scenes
+
+        void clearLastScene(std::vector<int> indexes);
+        void renderScene(int sceneIndex, bool force = false);
 };
