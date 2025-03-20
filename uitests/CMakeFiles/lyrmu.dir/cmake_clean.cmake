fbd7757c58a7f87a9215c82467dcedd2c8bf93c4
Stachenn <stanislawjano23@gmail.com>
2025-03-20 21:04:32 +0100

    Tests(): Testing cmake config

---
+++ uitests/CMakeFiles/lyrmu.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lyrmu.dir/src/main.cpp.o"
+  "CMakeFiles/lyrmu.dir/src/main.cpp.o.d"
+  "lyrmu"
+  "lyrmu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lyrmu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
